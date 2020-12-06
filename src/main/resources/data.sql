3ae82f7982029ae54bc6112cd44885511efc08b4
phuocdoan <ddoan@miu.edu>
2020-12-06 15:39:16 -0600

    Role: edit role_id Edit sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO ROLE(id, role) VALUES (1, 'ROLE_USER');
-INSERT INTO ROLE(id, role) VALUES (2, 'ROLE_ADMIN');
+INSERT INTO ROLE(role_id, role) VALUES (1, 'ROLE_USER');
+INSERT INTO ROLE(role_id, role) VALUES (2, 'ROLE_ADMIN');
 
-INSERT into USER (id, email, password, name, enable, profile_verified, registration_verified, user_type) values (1, 'user@miu.edu', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.', 'User', 1, 0, 0, 0);
-INSERT into USER (id, email, password, name, enable, profile_verified, registration_verified, user_type) values (2, 'admin@miu.edu', '$2a$12$Pdn2Mxp1c7loJguhLtfzp.RRSHREL8Sp.bsasHnQzCLt8TSJzqiE6', 'Admin', 1, 0, 0, 0);
+INSERT into USER(user_id, email, password, name, enable, profile_verified, registration_verified, user_type) values (1, 'user@miu.edu', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.', 'User', 1, 1, 1, 0);
+INSERT into USER(user_id, email, password, name, enable, profile_verified, registration_verified, user_type) values (2, 'admin@miu.edu', '$2a$12$Pdn2Mxp1c7loJguhLtfzp.RRSHREL8Sp.bsasHnQzCLt8TSJzqiE6', 'Admin', 1, 1, 1, 0);
 
 insert into user_roles(user_id, role_id) values (1, 1);
 insert into user_roles(user_id, role_id) values (2, 2);
