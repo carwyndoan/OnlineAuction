8d616b8382a7df366ef94f38af94e9f0eaefcc15
Thai Nguyen <ndt.rachkien@gmail.com>
2020-12-06 23:32:28 -0600

    - Updated because of changing the template structure

--- src/main/java/miu/edu/auction/controller/UserController.java
+++ src/main/java/miu/edu/auction/controller/UserController.java
@@ -3,8 +3,14 @@ package miu.edu.auction.controller;
 import miu.edu.auction.domain.User;
 import miu.edu.auction.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @RequestMapping("/admin")
@@ -23,4 +29,21 @@ public class UserController {
         User savedUser = userService.saveUserWithVerificationKey(user);
         return "/registration/success";
     }
+
+    @RequestMapping("/edituser/{id}")
+    public String edit(@PathVariable int id, Model model) {
+        userService.findById(id).ifPresent(u -> model.addAttribute("user", u));
+        return "registration/edit";
+    }
+
+    @RequestMapping("/listuser")
+    public String list(@RequestParam("size") int pageSize, @RequestParam("number") int pageNumber, Model model) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<User> pUser = userService.findUserListPaging(pageable);
+        model.addAttribute("list", pUser.getContent());
+        return "registration/list";
+    }
+
+
+
 }
--- src/main/java/miu/edu/auction/repository/UserRepository.java
+++ src/main/java/miu/edu/auction/repository/UserRepository.java
@@ -3,9 +3,11 @@ package miu.edu.auction.repository;
 
 import miu.edu.auction.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+//public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
     User findByEmail(String email);
 }
