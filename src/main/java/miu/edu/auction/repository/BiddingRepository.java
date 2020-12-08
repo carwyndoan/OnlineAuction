b7f8f9eb76d2815823c387b3b4ba0fc2609097da
Thai Nguyen <ndt.rachkien@gmail.com>
2020-12-07 20:16:24 -0600

    - Bidding of Winner

--- src/main/java/miu/edu/auction/repository/BiddingRepository.java
+++ src/main/java/miu/edu/auction/repository/BiddingRepository.java
@@ -1,22 +1,30 @@
 package miu.edu.auction.repository;
 
 import miu.edu.auction.domain.Bidding;
+import miu.edu.auction.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
-public interface BiddingRepository extends CrudRepository<Bidding, Integer> {
+public interface BiddingRepository extends JpaRepository<Bidding, Integer> {
 
     @Query("select bid from Bidding bid inner join Product p inner join Category c inner join User u " +
             "where c.category_id = :category_id and u.email <> :exclude_email")
     List<Bidding> findBiddingByCategory(Integer category_id, String exclude_email);
 
-    @Query(value = "select bid from User u join u.winBiddings bid")
-    List<Bidding> findByWinner(@Param("user_id")Integer user_id);
+    @Query("select bid from Bidding bid inner join fetch bid.winner u where u.email = :email")
+    List<Bidding> findByWinner(String email);
+
+
+//    @Query("select bid from Bidding bid")
+//    Collection<Bidding> findByWinner(@Param("user_id")Integer user_id);
+
 
 //    SELECT ph FROM Employee e JOIN e.phones ph WHERE ph LIKE '1%'
 }
