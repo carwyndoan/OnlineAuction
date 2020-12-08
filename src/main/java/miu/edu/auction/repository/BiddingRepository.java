f1f3ae3743d0953b3f6fbd2a207cd8b418c21a3f
Thai Nguyen <ndt.rachkien@gmail.com>
2020-12-08 00:43:03 -0600

    - Update payment based on the winning list

--- src/main/java/miu/edu/auction/repository/BiddingRepository.java
+++ src/main/java/miu/edu/auction/repository/BiddingRepository.java
@@ -22,19 +22,10 @@ public interface BiddingRepository extends JpaRepository<Bidding, Integer> {
             + "left join fetch bid.bidding_activities ba "
             + "where seller.email <> :exclude_email "
             + "and (:category_id = 0 or cat.category_id = :category_id)"
-            )
+    )
     List<Bidding> findBiddingByCategory(Integer category_id, String exclude_email);
 
-    @Query("select bid from Bidding bid inner join fetch bid.winner u where u.email = :email")
+    //    @Query("select bid from Bidding bid inner join fetch bid.winner u where u.email = :email")
+    @Query("select bid from Bidding bid inner join fetch bid.winner u inner join bid.payments p where u.email = :email and p.paymentDate is null")
     List<Bidding> findByWinner(String email);
-
-//    @Override
-//    Optional<Bidding> findById(Integer integer);
-
-
-    //    @Query("select bid from Bidding bid")
-//    Collection<Bidding> findByWinner(@Param("user_id")Integer user_id);
-
-
-//    SELECT ph FROM Employee e JOIN e.phones ph WHERE ph LIKE '1%'
 }
