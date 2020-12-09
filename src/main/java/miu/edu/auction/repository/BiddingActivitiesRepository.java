9c217014dd66bee41e395dc17ff0f00fab094042
phuocdoan <ddoan@miu.edu>
2020-12-08 22:26:16 -0600

    90%: Filter verified customer; Filter product for seller 80%: Close bidding; Pick the winner; Return deposit 50%: Deposit complete in auction system 50%: Return deposit; Seller receive full amount

--- src/main/java/miu/edu/auction/repository/BiddingActivitiesRepository.java
+++ src/main/java/miu/edu/auction/repository/BiddingActivitiesRepository.java
@@ -10,7 +10,15 @@ import org.springframework.stereotype.Repository;
 public interface BiddingActivitiesRepository extends JpaRepository<Bidding_Activities, Integer> {
 
     @Query(value = "select max(a.amount) from Bidding_Activities a "
-            + "inner join Bidding b "
-            + "where b.bidding_id = :bidding_id")
+            //+ "inner join Bidding b "
+            //+ "where b.bidding_id = :bidding_id"
+            + "where a.bidding.bidding_id = :bidding_id"
+    )
     Double getMaxBiddingActivitiesByBidding(Integer bidding_id);
+
+    @Query(value = "select ba from Bidding_Activities ba "
+            + "inner join fetch ba.bidding_user "
+        + "where ba.bidding.bidding_id = :bidding_id "
+        + "and ba.amount = :maxAmount")
+    Bidding_Activities getMaxBiddingActivity(Integer bidding_id, Double maxAmount);
 }
