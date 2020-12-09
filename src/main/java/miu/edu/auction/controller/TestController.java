7cabbe05540bbff2f5197a02d25cecabeef89781
Thai Nguyen <ndt.rachkien@gmail.com>
2020-12-08 19:42:45 -0600

    - Update payment based on the winning list

--- src/main/java/miu/edu/auction/controller/TestController.java
+++ src/main/java/miu/edu/auction/controller/TestController.java
@@ -8,6 +8,7 @@ import miu.edu.auction.service.BiddingService;
 import miu.edu.auction.service.PaymentService;
 import miu.edu.auction.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -15,9 +16,11 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.swing.text.html.Option;
 import javax.validation.Valid;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,7 +48,6 @@ public class TestController {
 
     @GetMapping(value = {"/paymentform/{id}"})
     public String loadPayment(@AuthenticationPrincipal UserDetails userDetails, @PathVariable Integer id, Model model) {
-//        int bID = Integer.parseInt(id);
         String userEmail = userDetails.getUsername();
         User user = userService.findUserByEmail(userEmail);
         Optional<Bidding> bidding = biddingService.findByID(id);
@@ -76,7 +78,28 @@ public class TestController {
         String userEmail = userDetails.getUsername();
         User user = userService.findUserByEmail(userEmail);
         List<BiddingActivityDTO> listDTO = biddingService.findBidingHistories(id);
+        int startYear = LocalDate.now().getYear();
+        List<Integer> months = new ArrayList<>();
+        List<Integer> years = new ArrayList<>();
+        for(int i = 1; i <=12; i++){
+            months.add(i);
+        }
+        int i = startYear - 10;
+        while (i <= startYear + 10){
+            years.add(i);
+            i++;
+        }
+        model.addAttribute("months", months);
+        model.addAttribute("years", years);
         model.addAttribute("activities", listDTO);
+        model.addAttribute("biddingid", id);
         return "bidding/BiddingHistories";
+    }
+
+    @GetMapping(value = {"/activitiesByMonthAndYear/{bidding_id}"})
+    public String loadHistoryByYearAndMonth(@PathVariable Integer bidding_id, @RequestParam Integer year, @RequestParam Integer month, Model model){
+
+        System.out.println(year + " " + month + " " + bidding_id);
+        return "registration/success";
     }
 }
