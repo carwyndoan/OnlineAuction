0ad2d6af261cffa18a00f9fd4d41bbe81602513f
Thai Nguyen <ndt.rachkien@gmail.com>
2020-12-09 10:30:49 -0600

    - Default after creating new User, enable is 0.

--- src/main/java/miu/edu/auction/utils/GenerationUnique.java
+++ src/main/java/miu/edu/auction/utils/GenerationUnique.java
@@ -1,18 +1,25 @@
 package miu.edu.auction.utils;
 
+import java.util.Random;
+
 public class GenerationUnique {
     /*
     generated a unique int by String
      */
     public static String hash(String s) {
-        int h = 0;
-        for (int i = 0; i < s.length(); i++) {
-            h = 31 * h + s.charAt(i);
+        Random rand = new Random();
+        long h = rand.nextInt(10);
+        int index = s.indexOf('@');
+//        System.out.println(index);
+        for (int i = 0; i < index; i++) {
+            h = 11 * h + s.charAt(i);
         }
         return String.valueOf(h);
     }
     public static void main(String[] args){
         String email = "ndt.rachkien@gmail.com";
+        System.out.println(GenerationUnique.hash(email));
+
         System.out.println(GenerationUnique.hash(email));
     }
 }
