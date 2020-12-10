9958a086c08c760736021078f294a9516785c1b2
Ragchaa <ragchaa1@gmail.com>
2020-12-09 19:54:30 -0600

    add customer search section

--- src/main/java/miu/edu/auction/controller/CustomerController.java
+++ src/main/java/miu/edu/auction/controller/CustomerController.java
@@ -1,14 +1,16 @@
 package miu.edu.auction.controller;
 
 import lombok.extern.java.Log;
+import miu.edu.auction.domain.Category;
 import miu.edu.auction.domain.Product;
+import miu.edu.auction.repository.CategoryRepository;
 import miu.edu.auction.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,14 +21,35 @@ public class CustomerController {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
     @GetMapping("/home")
     public String main(Model model) {
 
         List<Product> products = productRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+
         model.addAttribute("photos", products);
+        model.addAttribute("categories", categories);
 
         return "home2"; //view
+
     }
+
+    @GetMapping("/search")
+    public String search(Model model, @RequestParam("searchValue") String s) {
+
+        List<Product> products = productRepository.findByNameLike(s);
+        List<Category> categories = categoryRepository.findAll();
+
+        model.addAttribute("photos", products);
+        model.addAttribute("categories", categories);
+
+        return "home2"; //view
+
+    }
+
 
     @GetMapping("/detail")
 
--- src/main/java/miu/edu/auction/repository/ProductRepository.java
+++ src/main/java/miu/edu/auction/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package miu.edu.auction.repository;
 
 import miu.edu.auction.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findAll();
+
+    @Query("Select p from Product p where p.description like %:str%")
+    List<Product> findByNameLike(String str);
 }
