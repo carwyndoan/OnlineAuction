9698220ef44c747608af98552b1c0558c0285b13
Ragchaa <ragchaa1@gmail.com>
2020-12-09 22:39:34 -0600

    add customer search section

--- src/main/java/miu/edu/auction/repository/ProductRepository.java
+++ src/main/java/miu/edu/auction/repository/ProductRepository.java
@@ -3,13 +3,14 @@ package miu.edu.auction.repository;
 import miu.edu.auction.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    List<Product> findAll();
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+  //  List<Product> findAll();
 
     @Query("Select p from Product p where p.description like %:str%")
     List<Product> findByNameLike(String str);
