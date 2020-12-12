e83171698a06ab52aca600322486d0a4c90cc0b5
Thai Nguyen <ndt.rachkien@gmail.com>
2020-12-12 07:52:54 -0600

    - Add URL link to email content

--- src/main/java/miu/edu/auction/configuration/SecurityConfiguration.java
+++ src/main/java/miu/edu/auction/configuration/SecurityConfiguration.java
@@ -44,7 +44,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/registration","/login", "/security/**", "/h2-console/**").permitAll()
+                .antMatchers("/registration/*","/login", "/security/**", "/h2-console/**").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
                 //.antMatchers("/bidding/**","/bid/**").hasAuthority("ROLE_USER")
 //                .antMatchers("/seller/**").hasAuthority("ROLE_USER")
