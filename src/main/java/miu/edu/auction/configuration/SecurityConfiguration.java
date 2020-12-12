2478dbb6020e7657fbb49278f7fab93048a628d8
phuocdoan <ddoan@miu.edu>
2020-12-11 23:25:44 -0600

    Apply Jobrunr for Bidding, Payment

--- src/main/java/miu/edu/auction/configuration/SecurityConfiguration.java
+++ src/main/java/miu/edu/auction/configuration/SecurityConfiguration.java
@@ -43,25 +43,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-//        http.
-//                authorizeRequests()
-//                .antMatchers("/h2-console/**").permitAll()
-//                .antMatchers("/").permitAll()
-//                .antMatchers("/login").permitAll()
-////                .antMatchers("/registration").permitAll()
-////                .antMatchers("/admin/**").hasAuthority("ADMIN")
-//                .anyRequest().authenticated()
-//                .and().csrf().disable()
-//                .formLogin()
-//                .loginPage("/login").failureUrl("/login?error=true")
-//                //.defaultSuccessUrl("/admin/home")
-//                //.usernameParameter("email")
-//                //.passwordParameter("password")
-//                .and().logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/").and().exceptionHandling()
-//                .accessDeniedPage("/access-denied");
         http.authorizeRequests()
                 .antMatchers("/registration","/login", "/h2-console/**").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
